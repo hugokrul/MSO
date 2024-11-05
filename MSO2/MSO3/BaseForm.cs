dc788111534b976a9fdb9186dfde646b5e174814
hugokrul <hugo.krul@gmail.com>
2024-11-05 15:03:42 +0100

    exclude baseform from codecoverage

--- MSO2/MSO3/BaseForm.cs
+++ MSO2/MSO3/BaseForm.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MSO3
 {
+    [ExcludeFromCodeCoverage]
     public partial class BaseForm : Form
     {
         const string ApplicationName = "RoboLogic";
