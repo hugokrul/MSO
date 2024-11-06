aeeea1ca980d1d78c172480e4147d6c133ef487c
hugokrul <hugo.krul@gmail.com>
2024-11-06 15:45:11 +0100

    add positionTest

---
+++ MSO2/MSO2XUnit/PositionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MSO2;
+using Xunit;
+
+namespace MSO2XUnit
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void NextPositionTestSouth()
+        {
+            Board board = new(10, 10, 5, 5);
+            Position newPosition = board.Player.Position.GetNextPosition(Creature.Facing.South);
+            Assert.Equal(new Position(5, 6), newPosition);
+        }
+
+        [Fact]
+        public void NextPositionTestEast()
+        {
+            Board board = new(10, 10, 5, 5);
+            Position newPosition = board.Player.Position.GetNextPosition(Creature.Facing.East);
+            Assert.Equal(new Position(6, 5), newPosition);
+        }
+
+        [Fact]
+        public void NextPositionTestWest()
+        {
+            Board board = new(10, 10, 5, 5);
+            Position newPosition = board.Player.Position.GetNextPosition(Creature.Facing.West);
+            Assert.Equal(new Position(4, 5), newPosition);
+        }
+
+        [Fact]
+
+        public void NextPositionTestNorth()
+        {
+            Board board = new(10, 10, 5, 5);
+            Position newPosition = board.Player.Position.GetNextPosition(Creature.Facing.North);
+            Assert.Equal(new Position(5, 4), newPosition);
+        }
+    }
+}
